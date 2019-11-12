895b00ff079a99d122768b275fd547a2adf94bed
Jeff Dinkelman <mfankenstein@yahoo.com>
2019-11-12 09:03:52 -0800

    adds two challenges

---
+++ kyu8/find-the-integral.js
@@ -0,0 +1,9 @@
+// const integrate = (c,e) =>  {
+//   let newE = e + 1;
+//   let value = c / newE;
+//   return `${value}x^${newE}`;
+// };
+
+// const integrate = (c,e) => `${c / (e + 1)}x^${e + 1}`;
+
+const integrate = (c,e) => `${c / e += 1}x^${e}`;
